e69c3f3e0bf0685f09ef3ea478c1ffc63c350956
Diego Sueiro <Diego Sueiro>
2017-05-05 12:37:46 -0300

    Ejercicio 8

---
+++ Tps/1er_Tp/SQL/ejercicio8.sql
@@ -0,0 +1,6 @@
+select Pais.nombre, Equipo.nombre 
+	from Equipo 
+	inner join Competidor on Competidor.idEquipo = Equipo.idEquipo 
+	inner join Alumno on Alumno.idAlumno = Competidor.dniCompetidor 
+	inner join Escuela on Alumno.idEscuela = Escuela.idEscuela 
+	inner join Pais on Pais.idPais = Escuela.idPais;
