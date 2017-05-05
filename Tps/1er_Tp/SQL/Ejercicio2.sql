7523002f41bdc092c8ab9885f5f55c716ef0e0ec
jiro789 <ian.sabarros@mercadolibre.com>
2017-05-05 12:17:02 -0300

    cambio en order by

--- Tps/1er_Tp/SQL/Ejercicio2.sql
+++ Tps/1er_Tp/SQL/Ejercicio2.sql
@@ -1,9 +1,30 @@
 set sql_mode = '';
+-- QUERY MEDALLAS POR PAIS
+	
+-- puestos individuales por pais
+(select count(*) as cantidadMedallas, nombre  from 
 
--- QUERY ARBITROS POR PAIS
+((select  
+	case puestoCompetidor when 1 then 'ORO' when 2 then 'PLATA' when 3 then 'BRONCE' else NULL end as medalla,
+	Pais.nombre 
+	from Alumno 
+		inner join compiteEnCompetenciaInd on Alumno.idAlumno = compiteEnCompetenciaInd.dniCompetidor 
+			inner join Escuela on Alumno.idEscuela = Escuela.idEscuela 
+				inner join Pais on Escuela.idPais = Pais.idPais)
 
-select P.`nombre`, A.nombre, A.apellido from Arbitro as A 
-	inner join Pais as P on P.`idPais` = A.`idPais`;				
+union all
 
-				
-				
+-- puestos de team por pais
+(select 
+	-- Competidor.idEquipo, 
+	-- compiteEnCompetenciaTeam.idCompetencia,
+	case compiteEnCompetenciaTeam.puestoTeam when 1 then 'ORO' when 2 then 'PLATA' when 3 then 'BRONCE' else NULL end as medalla,
+	Pais.nombre 
+	from Alumno inner join Competidor on Alumno.idAlumno = Competidor.dniCompetidor 
+		inner join compiteEnCompetenciaTeam on Competidor.idEquipo = compiteEnCompetenciaTeam.idEquipo 
+			inner join Escuela on Alumno.idEscuela = Escuela.idEscuela 
+				inner join Pais on Escuela.idPais = Pais.idPais 
+				group by Competidor.idEquipo, compiteEnCompetenciaTeam.idCompetencia)) as R group by R.nombre order by cantidadMedallas desc limit 1);
+
+
+
